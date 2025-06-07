97504bbb8d589c10de29d66472c7be9a264115de
Juan Martín Román <maestroxd4@gmail.com>
2025-06-07 13:34:01 -0300

    Fix seguridad

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,15 @@
 <?php
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ImageUserController;
-use App\Http\Controllers\PublicacionesController;
-use App\Http\Controllers\ImagePublicacionController;
-use App\Http\Controllers\ChatController;
-use App\Http\Controllers\PlanesController;
-use App\Http\Controllers\MercadoPagoController;
-use App\Http\Controllers\CmsController;
-use App\Http\Controllers\NotificacionesController;
+use App\Http\Controllers\{ 
+    AuthController, UserController, ImageUserController, 
+    PublicacionesController, ImagePublicacionController, 
+    ChatController, PlanesController, MercadoPagoController, 
+    CmsController, NotificacionesController 
+};
 
-// Principal usuarios
+// --------------------- AUTENTICACIÓN ---------------------
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-
 Route::get('/auth/{provider}/redirect', [AuthController::class, 'redirect']);
 Route::get('/auth/{provider}/callback', [AuthController::class, 'callback']);
 
@@ -24,116 +17,137 @@ Route::middleware('auth:api')->group(function () {
   Route::get('get_credentials_from_token', [AuthController::class, 'get_credentials_from_token']);
 });
 
-// Data users sin token
+// --------------------- USUARIOS ---------------------
+
+// Públicos
 Route::get('user/obtenerUserCorreo/{correo}', [UserController::class, 'obtenerUserCorreo']);
 Route::get('user/obtenerTallas/{correo}', [UserController::class, 'obtenerTallasUser']);
 Route::post('user/getUsers/{page}', [UserController::class, 'getUsers']);
 Route::post('user/borrarCuenta/{user_id}', [UserController::class, 'borrarCuenta']);
 
-// Data para uno mismo (users token)
-Route::get('user/crearCodigoVerificacion/{correo}', [UserController::class, 'crearCodigoVerficacion']);
-Route::post('user/verificarCodigo/{correo}/{codigo}', [UserController::class, 'verficarCodigo']);
-Route::get('user/obtenerUserToken', [UserController::class, 'obtenerUserToken']);
-Route::post('user/actualizarPerfil', [UserController::class, 'completarPerfil']);
-Route::post('user/actualizarTallas', [UserController::class, 'actualizarTallasUser']);
-Route::post('user/actualizarEstilos', [UserController::class, 'actualizarEstilosUser']);
-Route::post('user/reportarPublicacion', [UserController::class, 'reportarPublicacion']);
-Route::post('user/reportarUsuario', [UserController::class, 'reportarUsuario']);
-Route::post('user/solicitarVerificado', [UserController::class, 'solicitarVerificado']);
+// Protegidos
+Route::middleware('auth:api')->prefix('user')->group(function () {
+    Route::get('crearCodigoVerificacion/{correo}', [UserController::class, 'crearCodigoVerficacion']);
+    Route::post('verificarCodigo/{correo}/{codigo}', [UserController::class, 'verficarCodigo']);
+    Route::get('obtenerUserToken', [UserController::class, 'obtenerUserToken']);
+    Route::post('actualizarPerfil', [UserController::class, 'completarPerfil']);
+    Route::post('actualizarTallas', [UserController::class, 'actualizarTallasUser']);
+    Route::post('actualizarEstilos', [UserController::class, 'actualizarEstilosUser']);
+    Route::post('reportarPublicacion', [UserController::class, 'reportarPublicacion']);
+    Route::post('reportarUsuario', [UserController::class, 'reportarUsuario']);
+    Route::post('solicitarVerificado', [UserController::class, 'solicitarVerificado']);
+});
 
+// Públicos (otros perfiles)
 Route::get('user/obtenerReseñas/{correo_user}', [UserController::class, 'obtenerReseñas']);
 Route::get('user/obtenerReseñasBasicas/{correo_user}', [UserController::class, 'obtenerReseñasBasicas']);
 Route::get('user/obtenerInformacion/{correo_user}', [UserController::class, 'obtenerInformacion']);
 Route::get('user/obtenerDescDeVc/{correo_user}', [UserController::class, 'obtenerDescDeVc']);
 
-// Data para notificaciones
-Route::get('notificaciones/obtener', [UserController::class, 'obtenerNotificaciones']);
-
-// Imagenes de usuarios
-Route::post('userImage/updateImage/{correo}/{isProfile?}', [ImageUserController::class, 'updateImage']);
-Route::get('userImage/getImages/{id}/{portada}', [ImageUserController::class, 'getImages'] );
-Route::get('userImage/getUserImageById/{user_id}', [ImageUserController::class, 'getUserImageById'] );
-Route::post('userImage/getImagesById', [ImageUserController::class, 'getImagesById']);
+// --------------------- NOTIFICACIONES ---------------------
+Route::middleware('auth:api')->get('notificaciones/obtener', [UserController::class, 'obtenerNotificaciones']);
 
 
-// Generales de publicacion 
-Route::post('publicaciones/crear', [PublicacionesController::class, 'crearPublicacion']);
-Route::post('publicaciones/eliminar/{publicacion_id}', [PublicacionesController::class, 'eliminarPublicacion']);
-Route::post('publicaciones/editar/{publicacion_id}', [PublicacionesController::class, 'editarPublicacion']);
-  // Especificas de publicacion
-
-    // Para home
-    Route::post('publicaciones/getPublicacionesRecomendadas/{page}', [PublicacionesController::class, 'getPublicacionesRecomendadas']);
-    Route::post('publicaciones/getPublicacionesGuardadas/{user_id}/{page}', [PublicacionesController::class, 'getPublicacionesGuardadas']);
-    Route::post('publicaciones/getPublicacionesExplorar/{user_id}/{page}', [PublicacionesController::class, 'getPublicacionesExplorar']);
-
-    // Para perfil
-    Route::post('publicaciones/getPublicacionesUser/{user_id}/{userProfile_id}/{page}', [PublicacionesController::class, 'getPublicacionesUser']);
-    Route::get('publicaciones/getPublicacionesGuardadasProfile/{page}', [PublicacionesController::class, 'getPublicacionesGuardadasProfile']);
-    Route::get('publicaciones/getPublicacionesEnVenta/{page}', [PublicacionesController::class, 'getPublicacionesEnVenta']);
-    Route::get('publicaciones/getPublicacionesEnCompra/{page}', [PublicacionesController::class, 'getPublicacionesEnCompra']);
-    
-    // Para buscador
-    Route::post('publicaciones/getPublicacionesFiltro', [PublicacionesController::class, 'getPublicacionesFiltro']);
-    
-    // Para cada publicacion
-    Route::get('publicaciones/getPublicacion/{user_id}/{publicacion_id}', [PublicacionesController::class, 'getPublicacion']);
-    Route::post('publicaciones/guardados/{publicacion_id}', [PublicacionesController::class, 'guardadosPublicacion']);
-    Route::post('publicaciones/finalizarConCalificacion/{publicacion_id}/{comprador?}', [PublicacionesController::class, 'finalizarConCalificacion']);
-
-// Imagenes de publicacion
-Route::get('publicaciones/getImageById/{publicacion_id}', [ImagePublicacionController::class, 'getPubImageById']);
-Route::get('publicacionImage/getFileImageById/{image_id}', [ImagePublicacionController::class, 'getFileImageById'] );
-Route::post('publicacionImage/updateImage/{publicacion_id}', [ImagePublicacionController::class, 'updateImage']);
-Route::post('publicacionesImage/getImagesById', [ImagePublicacionController::class, 'getImagesById']);
+// --------------------- IMÁGENES USUARIOS ---------------------
+Route::prefix('userImage')->group(function () {
+    Route::post('updateImage/{correo}/{isProfile?}', [ImageUserController::class, 'updateImage']);
+    Route::get('getImages/{id}/{portada}', [ImageUserController::class, 'getImages']);
+    Route::get('getUserImageById/{user_id}', [ImageUserController::class, 'getUserImageById']);
+    Route::post('getImagesById', [ImageUserController::class, 'getImagesById']);
+});
 
 
-// Chat 
-Route::post('chat/ofertar', [ChatController::class, 'ofertar']);
-Route::get('chat/obtenerChats', [ChatController::class, 'obtenerChats']);
-Route::get('chat/obtenerConversation/{conversation_id}', [ChatController::class, 'obtenerConversation']);
+// --------------------- PUBLICACIONES ---------------------
+Route::prefix('publicaciones')->group(function () {
+    // CRUD
+    Route::post('crear', [PublicacionesController::class, 'crearPublicacion']);
+    Route::post('eliminar/{publicacion_id}', [PublicacionesController::class, 'eliminarPublicacion']);
+    Route::post('editar/{publicacion_id}', [PublicacionesController::class, 'editarPublicacion']);
 
-// Ofertas a publicaciones
-Route::post('publicaciones/eliminarOferta', [PublicacionesController::class, 'eliminarOferta']);
+    // Home
+    Route::post('getPublicacionesRecomendadas/{page}', [PublicacionesController::class, 'getPublicacionesRecomendadas']);
+    Route::post('getPublicacionesGuardadas/{user_id}/{page}', [PublicacionesController::class, 'getPublicacionesGuardadas']);
+    Route::post('getPublicacionesExplorar/{user_id}/{page}', [PublicacionesController::class, 'getPublicacionesExplorar']);
 
-// Impulsar publicaciones
-Route::post('publicaciones/impulsar/{publicacion_id}', [PublicacionesController::class, 'impulsarPublicacion']);
+    // Perfil
+    Route::post('getPublicacionesUser/{user_id}/{userProfile_id}/{page}', [PublicacionesController::class, 'getPublicacionesUser']);
+    Route::get('getPublicacionesGuardadasProfile/{page}', [PublicacionesController::class, 'getPublicacionesGuardadasProfile']);
+    Route::get('getPublicacionesEnVenta/{page}', [PublicacionesController::class, 'getPublicacionesEnVenta']);
+    Route::get('getPublicacionesEnCompra/{page}', [PublicacionesController::class, 'getPublicacionesEnCompra']);
+
+    // Buscador
+    Route::post('getPublicacionesFiltro', [PublicacionesController::class, 'getPublicacionesFiltro']);
+
+    // Detalle y acciones
+    Route::get('getPublicacion/{user_id}/{publicacion_id}', [PublicacionesController::class, 'getPublicacion']);
+    Route::post('guardados/{publicacion_id}', [PublicacionesController::class, 'guardadosPublicacion']);
+    Route::post('finalizarConCalificacion/{publicacion_id}/{comprador?}', [PublicacionesController::class, 'finalizarConCalificacion']);
+    Route::post('eliminarOferta', [PublicacionesController::class, 'eliminarOferta']);
+    Route::post('impulsar/{publicacion_id}', [PublicacionesController::class, 'impulsarPublicacion']);
+
+    // Estadísticas
+    Route::get('estadisticas/getEstadisticas/{periodo}', [PublicacionesController::class, 'getEstadisticas']);
+});
 
 
-// Obtener planes o plan
-Route::get('planes/obtenerPlanes', [PlanesController::class, 'obtenerPlanes']);
-Route::get('planes/obtenerPlanActual', [PlanesController::class, 'obtenerPlanActual']);
-Route::post('planes/cancelarPlan', [PlanesController::class, 'cancelarPlan']);
+// --------------------- IMÁGENES PUBLICACIÓN ---------------------
+Route::prefix('publicacionImage')->group(function () {
+    Route::get('getImageById/{publicacion_id}', [ImagePublicacionController::class, 'getPubImageById']);
+    Route::get('getFileImageById/{image_id}', [ImagePublicacionController::class, 'getFileImageById']);
+    Route::post('updateImage/{publicacion_id}', [ImagePublicacionController::class, 'updateImage']);
+    Route::post('getImagesById', [ImagePublicacionController::class, 'getImagesById']);
+});
 
-// Obtener estadisticas
-Route::get('estadisticas/getEstadisticas/{periodo}', [PublicacionesController::class, 'getEstadisticas']);
 
-// Comprar plan
-Route::post('mercadopago/create', [MercadoPagoController::class, 'createPreference']);
-Route::post('mercadopago/confirm', [MercadoPagoController::class, 'confirmTransaction']);
+// --------------------- CHAT ---------------------
+Route::middleware('auth:api')->group(function () {
+  Route::get('chat/obtenerChats', [ChatController::class, 'obtenerChats']);
+  Route::get('chat/obtenerConversation/{conversation_id}', [ChatController::class, 'obtenerConversation']);
+  Route::post('chat/ofertar', [ChatController::class, 'ofertar']);
+  Route::post('chat/sendMessage', [ChatController::class, 'sendMessage']);
+  Route::post('chat/markAsRead/{conversationId}', [ChatController::class, 'markAsRead']);
+});
 
-// Cms
-Route::get('cms/getGeneralData', [CmsController::class, 'getGeneralData']);
-Route::get('cms/getNuevosUsuarios', [CmsController::class, 'getNuevosUsuarios']);
-Route::get('cms/getGanancias', [CmsController::class, 'getGanancias']);
-Route::post('cms/createInversion', [CmsController::class, 'createInversion']); 
-Route::get('cms/getInversiones', [CmsController::class, 'getInversiones']);
-Route::post('cms/eliminarInversion', [CmsController::class, 'eliminarInversion']); 
-Route::get('cms/getPublicacionesData', [CmsController::class, 'getPublicacionesData']);
-Route::get('cms/getVentasData', [CmsController::class, 'getVentasData']);
-Route::get('cms/getUsersSolicitudes', [CmsController::class, 'getUsersSolicitudes']);
-Route::post('cms/verificarUsuario', [CmsController::class, 'verificarUsuario']);
-Route::post('cms/rechazarVerificacion', [CmsController::class, 'rechazarVerificacion']);
+// --------------------- PLANES ---------------------
+Route::middleware('auth:api')->prefix('planes')->group(function () {
+    Route::get('obtenerPlanes', [PlanesController::class, 'obtenerPlanes']);
+    Route::get('obtenerPlanActual', [PlanesController::class, 'obtenerPlanActual']);
+    Route::post('cancelarPlan', [PlanesController::class, 'cancelarPlan']);
+});
 
-Route::get('cms/getReportes', [CmsController::class, 'getReportes']);
 
-Route::post('cms/eliminarReportePub', [CmsController::class, 'eliminarReportePub']);
-Route::post('cms/eliminarReporteUser', [CmsController::class, 'eliminarReporteUser']);
+// --------------------- MERCADO PAGO ---------------------
+Route::middleware('auth:api')->prefix('mercadopago')->group(function () {
+    Route::post('create', [MercadoPagoController::class, 'createPreference']);
+    Route::post('confirm', [MercadoPagoController::class, 'confirmTransaction']);
+});
 
-Route::post('cms/eliminarPub', [CmsController::class, 'eliminarPub']);
-Route::post('cms/eliminarUser', [CmsController::class, 'eliminarUser']);
 
-Route::post('notificacion/recibiste-oferta', [NotificacionesController::class, 'recibisteOferta']);
-Route::post('notificacion/aceptaste-oferta', [NotificacionesController::class, 'AceptasteOferta']);
-Route::post('notificacion/oferta-aceptada', [NotificacionesController::class, 'OfertaAceptada']);
-Route::post('notificacion/oferta-rechazada', [NotificacionesController::class, 'OfertaRechazada']);
+// --------------------- CMS ---------------------
+Route::middleware('auth:api')->prefix('cms')->group(function () {
+    Route::get('getGeneralData', [CmsController::class, 'getGeneralData']);
+    Route::get('getNuevosUsuarios', [CmsController::class, 'getNuevosUsuarios']);
+    Route::get('getGanancias', [CmsController::class, 'getGanancias']);
+    Route::get('getInversiones', [CmsController::class, 'getInversiones']);
+    Route::get('getPublicacionesData', [CmsController::class, 'getPublicacionesData']);
+    Route::get('getVentasData', [CmsController::class, 'getVentasData']);
+    Route::get('getUsersSolicitudes', [CmsController::class, 'getUsersSolicitudes']);
+    Route::get('getReportes', [CmsController::class, 'getReportes']);
+
+    Route::post('createInversion', [CmsController::class, 'createInversion']);
+    Route::post('eliminarInversion', [CmsController::class, 'eliminarInversion']);
+    Route::post('verificarUsuario', [CmsController::class, 'verificarUsuario']);
+    Route::post('rechazarVerificacion', [CmsController::class, 'rechazarVerificacion']);
+    Route::post('eliminarReportePub', [CmsController::class, 'eliminarReportePub']);
+    Route::post('eliminarReporteUser', [CmsController::class, 'eliminarReporteUser']);
+    Route::post('eliminarPub', [CmsController::class, 'eliminarPub']);
+    Route::post('eliminarUser', [CmsController::class, 'eliminarUser']);
+});
+
+// --------------------- NOTIFICACIONES ACCIONES ---------------------
+Route::middleware('auth:api')->prefix('notificacion')->group(function () {
+    Route::post('recibiste-oferta', [NotificacionesController::class, 'recibisteOferta']);
+    Route::post('aceptaste-oferta', [NotificacionesController::class, 'AceptasteOferta']);
+    Route::post('oferta-aceptada', [NotificacionesController::class, 'OfertaAceptada']);
+    Route::post('oferta-rechazada', [NotificacionesController::class, 'OfertaRechazada']);
+});
